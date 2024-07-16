b72e943d8dc76f25b3d4f63b088f5aa9990850cc
amna-khan-web <ms.amna21380@gmail.com>
2024-07-16 12:57:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+var username = prompt("What's your beautiful name?");
+alert("Welcome__" + username + "__to this Amazing quiz session!");
+alert("BEST OF LUCK!!!");
+
+//  Question #1
+
+var quiz1 = prompt("How do you write an IF statement in JavaScript___ A) if i = 5 then, B) if I == 5  then, C) if (i == 5) , D)  D) if i = 5");
+if (quiz1 === "C") {
+   alert(" Congratulation__! Your Answer is Correct____!!!");
+}
+else {
+   (quiz1 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #2
+
+var quiz2 = prompt(" What is the purpose of the alert() method in JavaScript? a) To prompt the user for input. b) To display a message in a dialog box. c) To log messages to the console. d) To write text into an HTML document.");
+if (quiz2 === "b") {
+   alert(" Congratulation__! Your Answer is Correct____!!!");
+}
+else {
+   (quiz2 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #3
+
+var quiz3 = prompt("Is it possible to close the alert dialog programmatically without user interaction? a) Yes, by using JavaScript to close the dialog. b) No, it requires user interaction to close. c) Yes, by using a special library. d) Yes, by using a browser-specific feature.");
+if (quiz3 === "b") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz3 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #4
+
+var quiz4 = prompt(" What does the Symbol data type represent in JavaScript? a) A unique and immutable primitive value. b) A mutable object. c) A method to handle asynchronous operations. d) A data structure for handling sets of values.");
+if (quiz4 === "a") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz4 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+//  Question #5
+
+var quiz5 = prompt("Can you customize the appearance of the alert dialog box (like changing colors or fonts) using CSS? a) Yes, by using external stylesheets. b) Yes, by using inline styles. c) No, the appearance cannot be customized. d) Yes, by using JavaScript properties.");
+if (quiz5 === "c") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz5 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//  Question #6
+
+var quiz6 = prompt("Is it possible to close the alert dialog programmatically without user interaction? a) Yes, by using JavaScript to close the dialog. b) No, it requires user interaction to close. c) Yes, by using a special library. d) Yes, by using a browser-specific feature.");
+if (quiz6 === "b") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz6 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//  Question #7
+
+var quiz7 = prompt("What is the purpose of the Set object in JavaScript? a) To store unique values of any type. b) To store key-value pairs. c) To create an ordered list of elements. d) To perform mathematical set operations.");
+if (quiz7 === "a") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz7 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #8
+
+var quiz8 = prompt("What does DOM stand for? A) Document Object Model. B) Data Object Model. C) Document Oriented Model. D) Data Oriented Model");
+if (quiz8 === "A") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz8 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//Question #9
+
+var quiz9 = prompt("What does the typeof operator in JavaScript return? A) The type of a variable. B) The value of a variable. C) The size of a variable. D) The name of a variable");
+if (quiz9 === "A") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz9 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//Question #10
+
+var quiz10 = prompt("Which of the following is not a primitive data type in Java? A) int. B) float. C) string. D) boolean");
+if (quiz10 === "C") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz10 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
